74f35d8a6d185c76c5514f51177204546610653f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-01 12:12:52 -0500

    add code to display avatar if first message by user is available

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,15 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
+
   return (
-    <div>
-      Their Message
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div className="message-avatar"
+        style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+        
+        />
+      )}
     </div>
   )
 };
