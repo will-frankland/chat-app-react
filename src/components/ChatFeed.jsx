4e26d243de3c80492de2adaa913172faba8d318d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-01 15:25:50 -0500

    render read receipts within block of code and pass message and isMyMessage down to it

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -47,8 +47,7 @@ const ChatFeed = (props) => {
               marginLeft: isMyMessage ? "0px" : "68px",
             }}
           >
-            {" "}
-            read-receipts
+          {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
       );
