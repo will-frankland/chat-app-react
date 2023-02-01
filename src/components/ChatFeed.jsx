add2df924b00b32f880977231f06ffabf026bb7e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-01 11:23:39 -0500

    use map function inside renderMessages in order to identify the last message and my messages

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,26 +1,25 @@
-import MessageForm from './MessageForm';
-import MyMesssage from './MyMessage';
-import TheirMessage from './TheirMessage';
-
+import MessageForm from "./MessageForm";
+import MyMesssage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-  const { chats, activeChat, userName, messages} = props;
+  const { chats, activeChat, userName, messages } = props;
 
   const chat = chats && chats[activeChat];
 
   const renderMessages = () => {
     const keys = Object.keys(messages);
 
-    console.log('keys', keys)
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1]
+      const isMyMessage = userName === message.sender.username;
+    })
   };
 
   renderMessages();
 
-return(
-  <div>
-    ChatFeed
-  </div>
-)
+  return <div>ChatFeed</div>;
 };
 
 export default ChatFeed;
