259a42bda030d119908c2433cf16958823233619
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-01 15:07:27 -0500

    add picture icon to chat typing area - import items from ant-design and add component in return

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
   const [value, setValue] = useState('');
@@ -30,6 +31,11 @@ const MessageForm = (props) => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <label htmlFor='upload-button'>
+        <span className='image-button'>
+          <PictureOutlined classID='picture-icon'/>
+        </span>
+      </label>
     </form>
   )
 }
