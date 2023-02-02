fc1bbd48e3621791447e6d26db153d8249344088
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-02 17:45:58 -0500

    add button to allow submit of username and password fields

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -16,6 +16,11 @@ const LoginForm = () => {
           <form onSubmit={handleSubmit}>
             <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} className='input' placeholder="Username" required />
             <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='input' placeholder="Password" required />
+            <div align='center'>
+              <button type="submit" className="button">
+                <span>Start Chatting</span>
+              </button>
+            </div>
           </form>
       </div>
 
