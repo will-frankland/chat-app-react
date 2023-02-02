ccabc68e7415a507e0c9afa85bce0b2d7061d413
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-02 17:42:07 -0500

    create divs with classNames and add form and input fields

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
+import axios from 'axios';
 
 const LoginForm = () => {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
-}
+
+  const handleSubmit = () => {
+
+  };
+
+  return(
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+          <form onSubmit={handleSubmit}>
+            <input type='text' value={username} onChange={(e) => setUsername(e.target.value)}>
+            </input>
+          </form>
+      </div>
+
+    </div>
+  )
+};
